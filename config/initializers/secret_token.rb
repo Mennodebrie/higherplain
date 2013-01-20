bccde6f7b8f9c5cd7504c415c092fc4a69b374bf
Menno de Brie <menno@paperchip.nl>
2013-01-20 16:57:25 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Higherplain::Application.config.secret_token = '06d79e18a0d8b1cfcb63e7c851c40d689a4947b409e94782c086853f6dc29c5c7910306dd00666afa330e5b451357050241ae87e307cd413ab4cd90926fcd128'
