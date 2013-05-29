16dc6dd01bd239d04f8978c7679e9fc9aef20062
Menno de Brie <menno@paperchip.nl>
2013-05-29 13:34:36 +0200

    Dragonfly initializer

--- config/initializers/refinery/images.rb
+++ config/initializers/refinery/images.rb
@@ -1,29 +1,29 @@
 # encoding: utf-8
 Refinery::Images.configure do |config|
   # Configures the maximum allowed upload size (in bytes) for an image
-  # config.max_image_size = 5242880
+  config.max_image_size = 5242880
 
   # Configure how many images per page should be displayed when a dialog is presented that contains images
-  # config.pages_per_dialog = 18
+  config.pages_per_dialog = 18
 
   # Configure how many images per page should be displayed when a dialog is presented that
   # contains images and image resize options
-  # config.pages_per_dialog_that_have_size_options = 12
+  config.pages_per_dialog_that_have_size_options = 12
 
   # Configure how many images per page should be displayed in the list of images in the admin area
-  # config.pages_per_admin_index = 20
+  config.pages_per_admin_index = 20
 
   # Configure image sizes
-  # config.user_image_sizes = {:small=>"110x110>", :medium=>"225x255>", :large=>"450x450>"}
+  config.user_image_sizes = {:small=>"110x110>", :medium=>"225x255>", :large=>"450x450>"}
 
   # Configure white-listed mime types for validation
-  # config.whitelisted_mime_types = ["image/jpeg", "image/png", "image/gif", "image/tiff"]
+  config.whitelisted_mime_types = ["image/jpeg", "image/png", "image/gif", "image/tiff"]
 
   # Configure image view options
-  # config.image_views = [:grid, :list]
+  config.image_views = [:grid, :list]
 
   # Configure default image view
-  # config.preferred_image_view = :grid
+  config.preferred_image_view = :grid
 
   # Configure S3 (you can also use ENV for this)
   # The s3_backend setting by default defers to the core setting for this but can be set just for images.
